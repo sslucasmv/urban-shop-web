950f26548e34900a9779628f0700502124a997f5
sslucasmv <lmvolpe24@gmail.com>
2024-07-13 12:31:38 -0300

    PRUEBA

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-      
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
        
         </p>
       </div>
       <p className="read-the-docs">
-  
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
