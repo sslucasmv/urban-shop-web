e40500f1dd6cd737a63eb274197bdeb8316fae04
sslucasmv <lmvolpe24@gmail.com>
2024-07-13 12:47:36 -0300

    PRUEBA 3

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-       
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-      hola
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
